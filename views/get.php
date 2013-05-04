894493516952f59cfd2a98b6924ff8d0794ef20c
Andreas Gohr <andi@splitbrain.org>
2013-05-04 19:18:03 +0200

    added todo, trying adsense

--- views/get.php
+++ views/get.php
@@ -52,6 +52,19 @@
                 </li>
                 <li>Read the <a href="https://www.dokuwiki.org/manual">DokuWiki manual</a></li>
             </ul>
+
+
+            <script type="text/javascript"><!--
+                google_ad_client = "ca-pub-5892664235245840";
+                /* download.dw.org */
+                google_ad_slot = "5306362121";
+                google_ad_width = 300;
+                google_ad_height = 250;
+                //-->
+            </script>
+            <script type="text/javascript"
+                    src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+            </script>
         </div>
 
         <div class="span4">
