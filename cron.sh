b57f4abcfea2f640aa1329f783272ef577fffbbd
Andreas Gohr <andi@splitbrain.org>
2013-05-09 13:11:53 +0200

    added cache cleaning to cron job

--- cron.sh
+++ cron.sh
@@ -3,11 +3,13 @@
 DIR=`dirname "$1"`
 cd "$DIR"
 
+# pull dokuwiki core
 cd src/dokuwiki/master
 pwd
 git pull origin master
 cd ../../../
 
+# pull plugins
 for PLUGIN in `find src/plugins/ -mindepth 1 -maxdepth 1 -type d`
 do
     cd $PLUGIN
@@ -15,3 +17,6 @@ do
     git pull origin master
     cd ../../../
 done
+
+# clear cache
+find out/*.tgz -name '*.tgz' -delete
