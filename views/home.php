373893ea352f288fe6947037ff3bd72f44f07f1a
Andreas Gohr <andi@splitbrain.org>
2020-11-09 19:40:44 +0100

    new micro apache version

--- views/home.php
+++ views/home.php
@@ -136,7 +136,7 @@
                                 <label class="checkbox">
                                     <input type="checkbox" name="onastick[]" value="mapache" />
                                     MicroApache (Windows)
-                                    <span class="hint">Apache 2.4.25, PHP 5.6.30, GD2 and SQLite</span>
+                                    <span class="hint">Apache 2.4.46, PHP 7.4.12, GD2 and SQLite</span>
                                 </label>
                             </li>
                         </ul>
